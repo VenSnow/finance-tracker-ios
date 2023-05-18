3ad03918c986e76920d97c244fd8baa3475a1f81
VenSnow <timhale2104@icloud.com>
2023-05-18 10:16:04 +0500

    Assets and some changes

---
+++ FinanceTracker/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  FinanceTracker
+//
+//  Created by Timur Gabdurashitov on 18/05/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
