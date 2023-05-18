cd3555030a7d85f514e2f5ba9a85506e08dbd8e1
VenSnow <timhale2104@icloud.com>
2023-05-18 08:35:09 +0500

    Initial Commit

---
+++ FinanceTracker/FinanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTrackerApp.swift
+//  FinanceTracker
+//
+//  Created by Timur Gabdurashitov on 18/05/23.
+//
+
+import SwiftUI
+
+@main
+struct FinanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
